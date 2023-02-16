99e1e763162bf591511cddf524f5cde34c7f4fb5
Luis Tellez <luis@luistellez.com>
2023-02-15 19:03:02 -0800

    feat: add interactivity to back and next buttons allowing them to change the current Month and Year

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,21 +1,40 @@
+import React from 'react';
 import './Calendar.scss';
 
+const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
 function Calendar() {
-  const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  // define the state for the current date using the JS Date object
+  const [currentDate, setCurrentDate] = React.useState(new Date());
 
-  // initialize date object using default Date constructor from JavaScript
-  const date = new Date();
+  // get the current year and month from the current date
+  const currentYearMonth = `${currentDate.getFullYear()} ${currentDate.toLocaleString('en-US', {
+    month: 'long',
+  })}`;
 
-  // get current year and month using getFullYear() and toLocaleString() methods
-  const currentYearMonth =
-    date.getFullYear() + ' ' + date.toLocaleString('en-US', { month: 'long' });
+  // define a function to handle the date change
+  const handleDateChange = (increment) => {
+    const newDate = new Date(currentDate);
+    newDate.setMonth(newDate.getMonth() + increment);
+    setCurrentDate(newDate);
+  };
 
   return (
     <div className='calendar'>
       <div className='calendar__header'>
         <div className='calendar__controls'>
-          <button className='calendar__button calendar__button_prev'>Back</button>
-          <button className='calendar__button calendar__button_next'>Next</button>
+          <button
+            className='calendar__button calendar__button_prev'
+            onClick={() => handleDateChange(-1)}
+          >
+            Back
+          </button>
+          <button
+            className='calendar__button calendar__button_next'
+            onClick={() => handleDateChange(1)}
+          >
+            Next
+          </button>
         </div>
         <h1 className='calendar__title'>{currentYearMonth}</h1>
       </div>
