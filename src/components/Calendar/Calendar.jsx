e161f1ec43a80b49e158d145357d5a1df47e6221
Luis Tellez <luis@luistellez.com>
2023-02-15 20:22:36 -0800

    feat: dinamically add boxes for each day including days from previous and next month to have a consistent full week experience

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -33,6 +33,41 @@ function Calendar() {
     localStorage.setItem('currentDate', currentDate);
   }, [currentDate]);
 
+  const currentMonthIndex = currentDate.getMonth();
+
+  // get the number of days in the current month
+  const daysInMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate();
+
+  // get the day of the week for the first day of the current month (0 = Sun, 1 = Mon, ..., 6 = Sat)
+  const firstDayOfWeek = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1).getDay();
+
+  // get the number of days in the previous month
+  const prevMonthLastDay = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0).getDate();
+
+  // render date boxes for the current month, previous month, and next month
+  const dateBoxes = Array.from(
+    { length: Math.ceil((daysInMonth + firstDayOfWeek) / 7) },
+    (week, i) => i,
+  ).map((week) => (
+    <div className='calendar__week' key={week}>
+      {Array.from({ length: 7 }, (week, i) => i).map((day) => {
+        const date = week * 7 + day + 1 - firstDayOfWeek;
+        const isCurrentMonth = currentDate.getMonth() === currentMonthIndex;
+        const isToday = currentDate.getDate() === date;
+        return (
+          <div
+            className={`calendar__day ${
+              !isCurrentMonth ? 'calendar__day_inactive' : isToday ? 'calendar__day_today' : ''
+            }`}
+            key={`${week}-${day}`}
+          >
+            {date < 1 ? prevMonthLastDay + date : date > daysInMonth ? date - daysInMonth : date}
+          </div>
+        );
+      })}
+    </div>
+  ));
+
   return (
     <div className='calendar'>
       <div className='calendar__header'>
@@ -63,6 +98,7 @@ function Calendar() {
             </div>
           ))}
         </div>
+        {dateBoxes}
       </div>
     </div>
   );
