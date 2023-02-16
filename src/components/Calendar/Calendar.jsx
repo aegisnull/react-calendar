8b980d0b46b52415dded0ef1a9e9c21878ebb684
Luis Tellez <luis@luistellez.com>
2023-02-15 17:43:31 -0800

    feat: add array containing name of days and using array.map add the days under the calendar__header

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,6 +1,8 @@
 import './Calendar.scss';
 
 function Calendar() {
+  const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
   return (
     <div className='calendar'>
       <div className='calendar__header'>
@@ -9,6 +11,15 @@ function Calendar() {
           <button className='calendar__button calendar__button_next'>Next</button>
         </div>
         <h1 className='calendar__title'>2023 February</h1>
+      </div>
+      <div className='calendar__body'>
+        <div className='calendar__week'>
+          {days.map((day) => (
+            <div className='calendar__day' key={day}>
+              {day}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
