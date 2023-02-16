0a2f24bf6fdecc442e5803b05193e08ce72e8fb5
Luis Tellez <luis@luistellez.com>
2023-02-15 16:49:42 -0800

    style: add calendar control buttons and add styling to them

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -4,6 +4,10 @@ function Calendar() {
   return (
     <div className='calendar'>
       <div className='calendar__header'>
+        <div className='calendar__controls'>
+          <button className='calendar__button calendar__button_prev'>Back</button>
+          <button className='calendar__button calendar__button_next'>Next</button>
+        </div>
         <h1 className='calendar__title'>2023 February</h1>
       </div>
     </div>
