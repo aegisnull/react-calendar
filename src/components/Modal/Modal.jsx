ecdbbb0f5df77a7a8167e5429863d0d2c4b05af8
Luis Tellez <luis@luistellez.com>
2023-02-17 22:36:35 -0800

    style: structure date for the Modal component

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,14 +1,16 @@
 import './Modal.scss';
 
 function Modal({ onClose, appointmentDetails }) {
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  const formattedDate = new Date(appointmentDetails.time).toLocaleString('en-US', options);
+
   return (
     <div className='modal'>
       <div className='modal__content'>
         <h1 className='modal__title'>Appointment</h1>
-        <p className='modal__appointment'>Date: {appointmentDetails.time}</p>
+        <p className='modal__appointment'>Date: {formattedDate}</p>
         <p className='modal__appointment'>Name: {appointmentDetails.name}</p>
         <div className='modal__buttons'>
-          <button className='modal__button'>Delete</button>
           <button className='modal__button' onClick={onClose}>
             Close
           </button>
