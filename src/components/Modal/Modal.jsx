3bf582bd5f1c32050886ae73733606b28732e49a
Luis Tellez <luis@luistellez.com>
2023-02-17 19:36:42 -0800

    feat: init Modal component

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,13 @@
+import './Modal.scss';
+
+function Modal() {
+  return (
+    <div className='modal'>
+      <div className='modal__content'>
+        <h1>Modal</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
