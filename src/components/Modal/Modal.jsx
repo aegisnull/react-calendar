5c0ce393792ae990a16f504365f4bdabca264cfe
Luis Tellez <luis@luistellez.com>
2023-02-17 19:39:57 -0800

    feat: finish HTML structure for Modal component

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -4,7 +4,10 @@ function Modal() {
   return (
     <div className='modal'>
       <div className='modal__content'>
-        <h1>Modal</h1>
+        <h1>Appointment</h1>
+        <p className='modal__appointment'>Appointment details</p>
+        <button className='modal__button'>Delete</button>
+        <button className='modal__button'>Close</button>
       </div>
     </div>
   );
