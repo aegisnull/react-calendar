abf20ae050a56719923a9497f7e00138c0db471e
Luis Tellez <luis@luistellez.com>
2023-02-17 20:26:01 -0800

    style: add styling to modal__title

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -4,7 +4,7 @@ function Modal({ onClose }) {
   return (
     <div className='modal'>
       <div className='modal__content'>
-        <h1>Appointment</h1>
+        <h1 className='modal__title'>Appointment</h1>
         <p className='modal__appointment'>Appointment details</p>
         <div className='modal__buttons'>
           <button className='modal__button'>Delete</button>
